0ea6fa34a3b8d2bce657f33fc298e875eec05d60
Ethan Gutmann <gutmann@ucar.edu>
2019-02-26 14:58:32 -0700

    update to post-processing tool to run much faster, fix a few bugs

--- helpers/post_proc_gard.py
+++ helpers/post_proc_gard.py
@@ -36,6 +36,8 @@ VERSION
 """
 from __future__ import absolute_import, print_function, division
 
+import time
+
 import sys
 import os
 import traceback
@@ -46,12 +48,16 @@ warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 import xarray as xr
+# from dask.distributed import Client; client = Client() # this breaks
 import numpy as np
 from scipy import stats
 
 global verbose
 verbose=False
 
+LUT_size = 100000
+x0 = np.arange(LUT_size)/(LUT_size-1.0)
+normal_LUT = stats.norm.ppf(x0)
 
 def process_file(varname = "pcp",
                  output_base = "output/gard_out_",
@@ -79,60 +85,75 @@ def process_file(varname = "pcp",
 
 
     if verbose: print("open the primary data files")
-    mean = xr.open_dataset(mean_file)
-    err = xr.open_dataset(err_file)
+    mean = xr.open_dataset(mean_file).load()
+    if verbose: print("load the errors file")
+    err = xr.open_dataset(err_file)[varname+"_error"].values.astype(np.float32)
+    if verbose: print("open the SCRF file")
     scrfs = xr.open_dataset(scrfs_file)
 
-    if verbose: print("apply the transform that was used within GARD")
+    if verbose: print("create the results dataset")
+    result = mean[varname].copy()
+
+
+    if verbose: print("apply the {} transform that was used within GARD".format(transform))
     if transform=="cuberoot":
-        d1 = np.cbrt(mean[varname])
+        d1 = np.cbrt(mean[varname].values.astype(np.float32))
     elif transform=="squareroot":
-        d1 = np.sqrt(mean[varname])
+        d1 = np.sqrt(mean[varname].values.astype(np.float32))
     elif transform=="log":
-        d1 = np.log(mean[varname])
+        d1 = np.log(mean[varname].values.astype(np.float32))
     elif transform=="None":
-        d1 = mean[varname]
+        d1 = mean[varname].values.astype(np.float32)
     else:
         raise KeyError("Unknown transform:"+transform)
 
 
-
-    if verbose: print("Get the random variables into a format that is appropriate in case the shapes don't match")
-    rand_tmp = scrfs.p_rand_uniform[scrf_offset:scrf_offset+d1.shape[0], :d1.shape[1], :d1.shape[2]]
-
     # if there is a probability of precipitation (PoP) or other threshold value specified
     # then we need to do some extra work to use that prediction
     if has_pop:
-        if verbose: print("load the PoP open_dataset")
-        pop = xr.open_dataset(pop_file)
-        popval = pop[varname+"_exceedence_probability"]
+        if verbose: print("Get the random variables into a format that is appropriate in case the shapes don't match")
+        rand_tmp = scrfs.p_rand_uniform.values[scrf_offset:scrf_offset+d1.shape[0], :d1.shape[1], :d1.shape[2]].astype(np.float32)
+
+        if verbose: print("load the PoP dataset")
+        pop = xr.open_dataset(pop_file).load()
+        popval = pop[varname+"_exceedence_probability"].values.astype(np.float32)
+
+        if verbose: print("Fix NaNs and +/-infs to 0")
+        popval[~np.isfinite(popval)] = 0
 
         if verbose: print("figure out where the probability was not exceeded")
-        noprecip = rand_tmp.values < (1-popval.values)
-        if verbose: print("rescale the random numbers to account for the limited portion of the cdf they should occupy")
-        rand_tmp.values -= (1-popval.values)
-        rand_tmp /= popval.values
+        noprecip = rand_tmp < (1-popval)
 
+        if verbose: print("rescale the random numbers to account for the limited portion of the cdf they should occupy")
+        rand_tmp -= (1-popval)
+        rand_tmp /= popval
+        rand_tmp[~np.isfinite(rand_tmp)] = 0
+
+        if verbose: print("threshold uniform random numbers")
+        rand_tmp[rand_tmp < 1e-5] = 1e-5
+        rand_tmp[rand_tmp > 0.99999] = 0.99999
         if verbose: print("convert uniform random numbers to normally distributed values")
-        rand_tmp.values[rand_tmp.values<=1e-20] = 1e-20
-        errors = stats.norm.ppf(rand_tmp.values)
+        # errors = stats.norm.ppf(rand_tmp)
+        idx = np.round(rand_tmp * (LUT_size-1.0)).astype(np.int32)
+        errors = normal_LUT[idx]
 
         # if you get a lot of errors from inifite numbers, uncomment these lines
         # errors[errors<-10] = -10
         # errors[errors>10] = 10
     else:
         if verbose: print("Getting error term")
-        errors = scrfs.t_rand[scrf_offset:scrf_offset+d1.shape[0], :d1.shape[1], :d1.shape[2]]
+        errors = scrfs.t_rand.values[scrf_offset:scrf_offset+d1.shape[0], :d1.shape[1], :d1.shape[2]].astype(np.float32)
 
     if verbose: print("compute the resulting value by added the error term and retransforming")
-    result = (d1 + (err[varname+"_error"].values * errors))
+    result.values = (d1 + (err * errors))
 
+    rdata = result.values.astype(np.float32)
     if transform=="cuberoot":
-        result = result**3
+        result.values = rdata * rdata * rdata
     elif transform=="squareroot":
-        result = result**2
+        result.values = rdata * rdata
     elif transform=="log":
-        result = np.exp(result)
+        result.values = np.exp(rdata)
     elif transform=="None":
         pass
     else:
@@ -141,8 +162,9 @@ def process_file(varname = "pcp",
 
     if has_pop:
         # the error term could set values that would otherwise have had precip to <0
+        if verbose: print("Setting negative values to 1e-10")
         result.values[result.values<1e-10] = 1e-10
-        if verbose: print("Mask any values that fell did not meet the threshold criteria")
+        if verbose: print("Mask any values that did not meet the threshold criteria")
         result.values[noprecip] = 0
 
     if verbose: print("Write the results to an output file")
